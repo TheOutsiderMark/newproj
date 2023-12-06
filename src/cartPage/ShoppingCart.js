a7e0626d7a9e234551e0fdf1584619cb57cb0f27
Imangali Kassymov <imangalikekov@gmail.com>
2023-12-06 18:04:09 +0100

    12345

--- src/cartPage/ShoppingCart.js
+++ src/cartPage/ShoppingCart.js
@@ -1,22 +1,25 @@
 import React, { useState, useEffect } from 'react';
 
 const ShoppingCart = ({ onClose, cartItems }) => {
-  const [token, setToken] = useState(localStorage.getItem('Bearer') || '');
+  
+  const [products, setProducts] = useState([]);
+  const token = localStorage.getItem('Bearer ');
 
-  useEffect(() => {
-    // Обновление состояния при изменении значения в localStorage
-    const handleStorageChange = () => {
-      setToken(localStorage.getItem('Bearer') || '');
-    };
+  fetch('http://localhost:8080/api/v1/products', {
+    method: 'GET', // или 'POST', 'PUT' и т.д.
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    }
+  })
+  .then(response => response.json())
+  .then(data => {
+    setProducts(data);
+  })
+  .catch(error => {
+    console.error('Ошибка аутентификации:', error);
+  });
 
-    // Подписка на событие изменения localStorage
-    window.addEventListener('storage', handleStorageChange);
-
-    // Отписка от события при размонтировании компонента
-    return () => {
-      window.removeEventListener('storage', handleStorageChange);
-    };
-  }, []);
 
   return (
     <div className="shopping-cart-container">
@@ -24,6 +27,18 @@ const ShoppingCart = ({ onClose, cartItems }) => {
         <h2>Shopping Cart</h2>
         <ul>
           {cartItems.map((item, index) => (
+            <li key={index}>
+              <img src={item.image} alt={item.title} />
+              <div>
+                <h3>{item.title}</h3>
+                <p>Description: {item.description}</p>
+                <p>Price: ${item.price}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+        <ul>
+        {products.map((item, index) => ( 
             <li key={index}>
               <img src={item.image} alt={item.title} />
               <div>
